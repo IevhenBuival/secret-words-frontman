8669e329a9c18c315b5919fead34d5e3f4780bcf
IevhenBuival <kdotbull@gmail.com>
2023-03-31 17:55:51 +0300

    regexp fix

--- src/components/AddEditWordDlg.tsx
+++ src/components/AddEditWordDlg.tsx
@@ -1,111 +1,149 @@
 import { Alert, Button, Form, Modal } from "react-bootstrap";
 
 import { useEffect, useState } from "react";
-import { useForm } from 'react-hook-form';
-import { AccessError, ConflictError, InvalidId, NotFoundError } from "../errors/http_errors";
+import { useForm } from "react-hook-form";
+import {
+  AccessError,
+  ConflictError,
+  InvalidId,
+  NotFoundError,
+} from "../errors/http_errors";
 import * as WordApi from "../hooks/words_api";
 import { IWord } from "../models/word";
 import TextInputFields from "./Form/TextInputFields";
 
 import { ILanguage } from "../models/language";
 
-
 interface IEditAddWordDialog {
-    WordToEdit?: IWord,
-    onDismiss: () => void,
-    onWordSave: (word: IWord) => void,
-    languages: ILanguage[],
-    currentLanguage?: string;
+  WordToEdit?: IWord;
+  onDismiss: () => void;
+  onWordSave: (word: IWord) => void;
+  languages: ILanguage[];
+  currentLanguage?: string;
 }
 
-const AddEditWordDlg = ({ WordToEdit, onDismiss, onWordSave, languages, currentLanguage }: IEditAddWordDialog) => {
+const AddEditWordDlg = ({
+  WordToEdit,
+  onDismiss,
+  onWordSave,
+  languages,
+  currentLanguage,
+}: IEditAddWordDialog) => {
+  const [showError, SetShowError] = useState<string | null>(null);
 
-    const [showError, SetShowError] = useState<string | null>(null);
-
-    const [selectedValue, setSelectedValue] = useState<string | undefined>(WordToEdit?.language || currentLanguage ? currentLanguage : languages ? languages[0]._id : '');
-
-    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<WordApi.IWordInput>({
-        defaultValues: {
-            title: WordToEdit?.title || "",
-            language: WordToEdit?.language || selectedValue,
-        }
-    });
-
-    useEffect(() => {
-        SetShowError(null);
-    }, [register])
-
-    async function onSubmit(input: WordApi.IWordInput) {
-
-        try {
-            let wordResponce: IWord;
-            if (WordToEdit) {
-                wordResponce = await WordApi.updateWord(WordToEdit._id, input);
-            } else {
-                wordResponce = await WordApi.createWord(input);
-            }
-            onWordSave(wordResponce);
-        } catch (error) {
-            if ((error instanceof AccessError) || (error instanceof ConflictError) || (error instanceof InvalidId) || (error instanceof NotFoundError)) {
-                SetShowError(error.message);
-            } else {
-
-                alert(error);
-            }
-            console.error(error);
-        }
+  function calcInitialState(): string | undefined {
+    let testedvalue: string | undefined = undefined;
+    if (testedvalue === undefined) {
+      if (WordToEdit?.language !== undefined)
+        testedvalue = WordToEdit?.language;
+      else if (currentLanguage) testedvalue = currentLanguage;
+      else if (languages) testedvalue = languages[0]._id;
+      else testedvalue = "";
     }
+    console.log("testedvalue-" + testedvalue);
+    return testedvalue;
+  }
+  const [selectedValue, setSelectedValue] = useState<string | undefined>(
+    calcInitialState()
+  );
 
-    return (<Modal show onHide={onDismiss}>
-        <Modal.Header closeButton>
-            <Modal.Title>
-                {WordToEdit ? 'Edit word card' : 'Add word card'}
-            </Modal.Title>
-        </Modal.Header>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<WordApi.IWordInput>({
+    defaultValues: {
+      title: WordToEdit?.title || "",
+      language: WordToEdit?.language || selectedValue,
+    },
+  });
 
-        <Modal.Body>
-            {showError && <Alert variant="danger">
-                {showError}
-            </Alert>}
-            {errors.title && <Alert variant="danger">{errors.title?.message}</Alert>}
-            <Form id="AddEditWordForm" onSubmit={handleSubmit(onSubmit)}>
-                <TextInputFields
-                    name='title'
-                    label="Title"
-                    type="text"
-                    placeholder="Title"
-                    register={register}
-                    registerOptions={selectedValue === '63f2400bc1ee59dffb6258ff' ? {
-                        pattern: { value: /[A-Za-z]/, message: 'Character does not correspond to the language ' }, required: { value: true, message: 'Fill in the title' }
-                    } : { pattern: { value: /[А-Яа-я]/, message: 'Літера не відповідає мові' }, required: { value: true, message: 'Заповніть назву' } }}
-                    error={errors.title}
-                />
+  useEffect(() => {
+    SetShowError(null);
+  }, [register]);
+  /*
+  useEffect(() => {
+    setWordPattern(ReturnPattern(selectedValue));
+  }, [selectedValue]);
+*/
+  async function onSubmit(input: WordApi.IWordInput) {
+    try {
+      let wordResponce: IWord;
+      if (WordToEdit) {
+        wordResponce = await WordApi.updateWord(WordToEdit._id, input);
+      } else {
+        wordResponce = await WordApi.createWord(input);
+      }
+      onWordSave(wordResponce);
+    } catch (error) {
+      if (
+        error instanceof AccessError ||
+        error instanceof ConflictError ||
+        error instanceof InvalidId ||
+        error instanceof NotFoundError
+      ) {
+        SetShowError(error.message);
+      } else {
+        alert(error);
+      }
+      console.error(error);
+    }
+  }
 
-                <Form.Label className="mb-3 mt-3">
-                    Language
-                </Form.Label>
-                <Form.Select aria-label="Default select example"
-                    {...register("language")}
+  return (
+    <Modal show onHide={onDismiss}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          {WordToEdit ? "Edit word card" : "Add word card"}
+        </Modal.Title>
+      </Modal.Header>
 
-                    onChange={(e) => {
-                        setSelectedValue(e.target.value);
+      <Modal.Body>
+        {showError && <Alert variant="danger">{showError}</Alert>}
+        {errors.title && (
+          <Alert variant="danger">{errors.title?.message}</Alert>
+        )}
+        <Form id="AddEditWordForm" onSubmit={handleSubmit(onSubmit)}>
+          <TextInputFields
+            name="title"
+            label="Title"
+            type="text"
+            placeholder="Title"
+            register={register}
+            langForPattern={selectedValue}
+            registerOptions={{
+              required: {
+                value: true,
+                message:
+                  selectedValue === "63f2400bc1ee59dffb6258ff"
+                    ? "Fill in the title"
+                    : "Заповніть назву",
+              },
+            }}
+            error={errors.title}
+          />
 
-                    }}
-                >
-                    {languages.map(l => <option value={l._id}>{l.name}</option>)}
-                </Form.Select>
-            </Form>
-        </Modal.Body>
-        <Modal.Footer>
-            <Button
-                form="AddEditWordForm"
-                type="submit"
-                disabled={isSubmitting}
-            >
-                Save
-            </Button>
-        </Modal.Footer>
-    </Modal>);
-}
+          <Form.Label className="mb-3 mt-3">Language</Form.Label>
+          <Form.Select
+            aria-label="Default select example"
+            {...register("language")}
+            onChange={(e) => {
+              setSelectedValue(e.target.value);
+            }}
+          >
+            {languages.map((l) => (
+              <option value={l._id}>{l.name}</option>
+            ))}
+          </Form.Select>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button form="AddEditWordForm" type="submit" disabled={isSubmitting}>
+          Save
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
 export default AddEditWordDlg;
--- src/components/Form/TextInputFields.tsx
+++ src/components/Form/TextInputFields.tsx
@@ -1,33 +1,68 @@
 import { Form } from "react-bootstrap";
 import { FieldError, RegisterOptions, UseFormRegister } from "react-hook-form";
-import Styles from '../../styles/WordPage.module.css'
+import Styles from "../../styles/WordPage.module.css";
 interface ITextInputFields {
-    name: string,
-    label: string,
-    register: UseFormRegister<any>,
-    registerOptions?: RegisterOptions,
-    error?: FieldError,
-    hiden?: boolean,
-    [x: string]: any,
+  name: string;
+  label: string;
+  register: UseFormRegister<any>;
+  registerOptions?: RegisterOptions;
+  langForPattern?: string;
+  error?: FieldError;
+  hiden?: boolean;
+  [x: string]: any;
 }
 
-const TextInputFields = ({ name, hiden, label, register, registerOptions, error, ...props }: ITextInputFields) => {
-    return (<Form.Group className="mt-3" controlId={name + "-input"}>
-        <Form.Label className="mb-3">
-            {label}
-        </Form.Label>
-        <div className={hiden ? Styles.hide : ''}>
-            <Form.Control
-                {...props}
-                isInvalid={!!error}
-                {...register(name, registerOptions)}
-            />
-        </div>
+const TextInputFields = ({
+  name,
+  hiden,
+  label,
+  register,
+  langForPattern,
+  registerOptions,
+  error,
+  ...props
+}: ITextInputFields) => {
+  interface WordPattern {
+    value: RegExp;
+    message: string;
+  }
+  function ReturnPattern(code: string | undefined): WordPattern {
+    return code === "63f2400bc1ee59dffb6258ff"
+      ? {
+          value: /[A-Za-z]+$/,
+          message: "Character does not correspond to the language ",
+        }
+      : {
+          value: /[А-Яа-я]+$/,
+          message: "Літера не відповідає мові",
+        };
+  }
 
-        <Form.Control.Feedback type="invalid">
-            {error?.message}
-        </Form.Control.Feedback>
-    </Form.Group>);
-}
+  console.log(registerOptions);
+  return (
+    <Form.Group className="mt-3" controlId={name + "-input"}>
+      <Form.Label className="mb-3">{label}</Form.Label>
+      <div className={hiden ? Styles.hide : ""}>
+        <Form.Control
+          {...props}
+          isInvalid={!!error}
+          {...register(
+            name,
+            langForPattern
+              ? {
+                  ...registerOptions,
+                  pattern: ReturnPattern(langForPattern),
+                }
+              : registerOptions
+          )}
+        />
+      </div>
+
+      <Form.Control.Feedback type="invalid">
+        {error?.message}
+      </Form.Control.Feedback>
+    </Form.Group>
+  );
+};
 
 export default TextInputFields;
